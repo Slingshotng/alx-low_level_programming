7f5ecaccbeec967292ac79d7f9e9a4952cfba593
David Mobolade <dmobolade@gmail.com>
2022-12-13 16:32:38 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#include <unistd.h>
+
+
+
+/**
+ *
+ *   * main - Entry point
+ *
+ *     *
+ *
+ *       * Return: Always 0
+ *
+ *         */
+
+int main(void)
+
+{
+
+		_putchar('_');
+
+		_putchar('p');
+
+		_putchar('t');
+
+		_putchar('c');
+
+		_putchar('h');
+
+		_putchar('a');
+
+		_putchar('r');
+
+		_putchar('\n');
+
+		return (0);
+}
